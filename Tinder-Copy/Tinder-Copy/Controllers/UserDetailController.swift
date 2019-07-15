b0e900e08fc7be7ad1b76f3fccf5d92748239b17
Kirill Ivanov <ivanofff.k@gmail.com>
2019-07-16 00:41:04 +0300

    Implement Stretchy header in Detail

--- Tinder-Copy/Tinder-Copy/Controllers/UserDetailController.swift
+++ Tinder-Copy/Tinder-Copy/Controllers/UserDetailController.swift
@@ -9,7 +9,52 @@
 import UIKit
 
 class UserDetailController: UIViewController {
+    
+    fileprivate lazy var scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.alwaysBounceVertical = true
+        sv.contentInsetAdjustmentBehavior = .never
+        sv.delegate = self
+        return sv
+    }()
+    
+    fileprivate let imageView: UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    fileprivate let infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name 30\nDoctor\nSome Bio"
+        label.numberOfLines = 0
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(scrollView)
+        scrollView.fillSuperview()
+        scrollView.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width)
+        scrollView.addSubview(infoLabel)
+        infoLabel.addConsctraints(scrollView.leadingAnchor, scrollView.trailingAnchor, imageView.bottomAnchor, nil, .init(top: 16, left: 16, bottom: 0, right: 16))
+        view.backgroundColor = .white
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+    
+    @objc fileprivate func handleTap() {
+        dismiss(animated: true)
+    }
+}
+
+extension UserDetailController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let changeY = scrollView.contentOffset.y
+        let width = view.frame.width - changeY * 2
+        if changeY < 0 {
+            imageView.frame = CGRect(x: changeY, y: changeY, width: width, height: width)
+        }
     }
 }
